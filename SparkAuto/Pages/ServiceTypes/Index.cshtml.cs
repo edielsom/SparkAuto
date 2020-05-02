a3d7dcd4c133dde4f4c17f860b592460d63f8ef4
Edielson <edielsom@hotmail.com>
2020-05-01 21:23:27 -0400

    Criando o Front-End ServiceType Index
    Foi implementado a tela para carregar as informações dos tipos de serviço.

---
+++ SparkAuto/Pages/ServiceTypes/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SparkAuto.Data;
+using SparkAuto.Models;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SparkAuto.Pages.ServiceTypes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+        public IList<ServiceType> ServiceType;
+        public IndexModel(ApplicationDbContext db)
+        {
+            this._db = db;
+            this.ServiceType = new List<ServiceType>();
+        }
+
+        public async Task<ActionResult> OnGet()
+        {
+            await this._db.ServiceTypes.ToListAsync();
+            return Page();
+        }
+    }
+}
